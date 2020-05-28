1a4958d5cc630194e922bd51e19a3569ba66ed9c
boris <1401749067@qq.com>
2020-05-28 17:26:18 +0800

    spring-BeanLifeDemo

---
+++ Spring-Bean—life/src/main/java/com/bjsxt/pojo/User.java
@@ -0,0 +1,31 @@
+package com.bjsxt.pojo;
+
+public class User {
+	private int id;
+	private String name;
+	private Integer age;
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
